6e5b5aa555abfb9a4171be542630a60e16ffdabf
frehiywot nega <fr.nega@gmail.com>
2022-10-26 14:38:47 +0300

    added read me and main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const bookList = document.querySelector('#displayed-books');
-const bookAdd = document.querySelector('.books-adding');
-const contactMe = document.querySelector('#contact-me');
+const bookAdd = document.querySelector('.add-awesombooks');
+const contactMe = document.querySelector('.contact-me');
 const link = document.querySelectorAll('.link');
-const link2 = document.querySelector('.addNew');
-const link1 = document.querySelector('.bookListing');
+const link2 = document.querySelector('.addbookform');
+const link1 = document.querySelector('.listofbook');
 const link3 = document.querySelector('.contactInfo');
 
 link.forEach((item) => {
   item.addEventListener('click', (e) => {
-    if (e.target.classList.contains('bookListing')) {
+    if (e.target.classList.contains('listofbook')) {
       bookAdd.style.display = 'none';
       contactMe.style.display = 'none';
       bookList.style.display = 'block';
@@ -16,7 +16,7 @@ link.forEach((item) => {
       link2.classList.remove('active');
       link3.classList.remove('active');
     }
-    if (e.target.classList.contains('addNew')) {
+    if (e.target.classList.contains('addbookform')) {
       contactMe.style.display = 'none';
       bookList.style.display = 'none';
       bookAdd.style.display = 'flex';
